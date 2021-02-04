5497ecb1690377ebd175819eb791b94d4395ffc3
she2489iiy <70935703+she2489iiy@users.noreply.github.com>
2021-02-04 12:30:51 +0800

    Add files via upload

---
+++ test/js/pictest.js
@@ -0,0 +1,25 @@
+$(function(){
+	setScroll();
+})
+
+function setScroll(){
+	var win = $(window);
+	var hWin = win.height();
+	var wWin = win.width();
+	var scroll = $(window).scrollTop();
+
+	var move = $(".picture_box");
+
+
+	move.each(function(){
+		var tCont = $(this).offset().top;
+
+		
+		if(scroll > tCont - hWin*0.85){
+			$(this).addClass("show");
+		}else{
+			$(this).removeClass("show");
+		}
+
+	})
+}
